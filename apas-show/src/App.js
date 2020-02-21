5e2e643ca9ffb97b7f62936e596c27c90222bdbd
jonatas mattos <jonidefjan@gmail.com>
2020-02-21 18:44:55 -0300

    0.3

--- apas-show/src/App.js
+++ apas-show/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 import Header from './components/Header/Index';
---
+++ apas-show/src/pages/congresso/index.js
@@ -0,0 +1,18 @@
+import React, {Component } from 'react';
+
+const Congresso = () =>(
+  
+  <Container>
+    <Row>
+      <Col>1 of 2</Col>
+      <Col>2 of 2</Col>
+    </Row>
+    <Row>
+      <Col>1 of 3</Col>
+      <Col>2 of 3</Col>
+      <Col>3 of 3</Col>
+    </Row>
+  </Container>
+
+);
+export default Congresso;
